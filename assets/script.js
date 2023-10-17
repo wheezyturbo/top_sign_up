a7de172f3e8b542ceb6173dfe2bfe9ec4403c949
rubyCiide <joelpaul900@gmail.com>
2023-10-17 17:48:26 +0530

    add script.js, validation for password match, login href

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("form");
+
+const invalid = document.querySelector(".invalid");
+
+form.addEventListener("click", (e) => {
+  e.preventDefault();
+  const pass = document.getElementById("password");
+  const conf = document.getElementById("confirmpassword");
+  if (pass === conf) {
+    invalid.textContent = "";
+    e.submit();
+  } else {
+      invalid.textContent = "*Passwords doesn't match";
+    setTimeout(()=>{
+        invalid.textContent ="";
+    },3000)
+  }
+});
